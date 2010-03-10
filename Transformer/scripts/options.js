8d8b093bba6c0bbb1dec2814fd692d0515e7eeb1
burgerkingorama <burgerkingorama@76a534cc-29ec-11df-a7bd-71b82df430f8>
2010-03-10 20:33:24 +0000

    save_options function now works
    git-svn-id: https://trnsfrmr.googlecode.com/svn/trunk@38 76a534cc-29ec-11df-a7bd-71b82df430f8

--- Transformer/scripts/options.js
+++ Transformer/scripts/options.js
@@ -9,8 +9,8 @@
 		{
 			el[i].innerHTML =  text;
 		}
-		
 	}
+	
 	function localize(elementname, messageid)
 	{
 		localizeString(elementname, chrome.i18n.getMessage(messageid) );
@@ -44,28 +44,43 @@
 	
 	function del(button)
 	{
-		button.parentElement.parentElement.outerHTML='';
+		var row = button.parentElement.parentElement;
+		
+		row.outerHTML="";
 	}
-	
+
+	function getElementsByClassName(classname, node)
+	{
+		if(!node) node = document.getElementsByTagName("body")[0];
+		var a = [];
+		var re = new RegExp('\\b' + classname + '\\b');
+		var els = node.getElementsByTagName("*");
+		
+		for(var i=0,j=els.length; i<j; i++)
+			if(re.test(els[i].className))a.push(els[i]);
+			
+		return a;
+	}
 	
 	// Saves options to localStorage.
 	function save_options()
 	{
-
-		var map = new Map;
-		map
-		.put('mp', 'Max Power')
-		.put('mfg', 'Mit freundlichen Grüßen')
-		.put('hp', 'Sehr geehrter Herr Prof. Dr.');
+		var subs = document.getElementById("subs");
 		
+		var lines = getElementsByClassName("sub_line", subs);
+	
 		var a = new Array();
 		
-		for(var j = 0; j++ < map.size; map.next())
-		{ // check all keys
-			a.push(map.key());
-			a.push(map.value());
-		}		
-
+		for(var i=0; i<lines.length; i++)
+		{
+			var key = getElementsByClassName("sub_key", lines[i])[0].value;
+			var value = getElementsByClassName("sub_value", lines[i])[0].value;
+			
+			a.push(key);
+			a.push(value);
+			// console.log(key + ", " + value);
+		}
+	
 		localStorage["map"] = JSON.stringify(a);
 	}
 	
