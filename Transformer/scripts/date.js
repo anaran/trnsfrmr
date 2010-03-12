3da64a473e3614ed0441cd4872361d95a461d265
Benjamin.Lehejcek <Benjamin.Lehejcek@76a534cc-29ec-11df-a7bd-71b82df430f8>
2010-03-12 22:25:51 +0000

    format date: getNewDate method broken
    git-svn-id: https://trnsfrmr.googlecode.com/svn/trunk@83 76a534cc-29ec-11df-a7bd-71b82df430f8

--- Transformer/scripts/date.js
+++ Transformer/scripts/date.js
@@ -10,8 +10,8 @@ function currentTime() {
 function replaceDates(value) {
 
 	var pDay = /%DAY(\+\d+)?%/;
-	var pMonth = /%MONTH%/;
-	var pYear = /%YEAR%/;
+	var pMonth = /%MONTH(\+\d+)?%/;
+	var pYear = /%YEAR(\+\d+)?%/;
 
 	var pd = new RegExp(pDay);
 	var pm = new RegExp(pMonth);
@@ -19,25 +19,26 @@ function replaceDates(value) {
 	
 	while ((md = pd.exec(value)) || (mm = pm.exec(value)) || (my = py.exec(value)))
 	{
-		d = getNewDate(getAddOn(pDay,value));
+		d = getNewDate(getAddOn(pDay,value), getAddOn(pMonth,value), getAddOn(pYear,value));
 		if (md)	{
 			value = value.replace(pDay , (d.getDate()<10)?"0"+d.getDate():d.getDate());
-		}
-		if (mm) {
+		} if (mm) {
 			month = d.getMonth()+1;
 			value = value.replace(pMonth , (month<10)?"0"+month: month);
-		}
-		if (my) {
+		} if (my) {
 			value = value.replace(pYear , d.getFullYear());
 		}
 	}
 	return value;
 }
 
-function getNewDate(days){
+function getNewDate(days, month, years){
 	var now = new Date();
-	var daylength= 86400000; //1*24*60*60*1000;
-	return new Date(now*1+daylength*days);
+	future = new Date();
+	future.setDate(now.getDate() + days);
+	future.setMonth(future.getMonth() + month);
+	future.setYear(future.getFullYear() + years);
+	return future;
 }
 
 function getAddOn(pattern, value) {
