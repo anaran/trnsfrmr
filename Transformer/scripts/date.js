8edeabd7ac2741ffae2670efcb013b9ef4ff824b
Benjamin.Lehejcek <Benjamin.Lehejcek@76a534cc-29ec-11df-a7bd-71b82df430f8>
2010-03-12 21:15:14 +0000

    change date formater RegExp
    git-svn-id: https://trnsfrmr.googlecode.com/svn/trunk@80 76a534cc-29ec-11df-a7bd-71b82df430f8

--- Transformer/scripts/date.js
+++ Transformer/scripts/date.js
@@ -8,9 +8,8 @@ function currentTime() {
 }
 
 function replaceDates(value) {
-	var d = new Date();
 
-	var pDay = /%DAY%(\(\d+\))?/;
+	var pDay = /%DAY(\+\d+)?%/;
 	var pMonth = /%MONTH%/;
 	var pYear = /%YEAR%/;
 
@@ -18,28 +17,32 @@ function replaceDates(value) {
 	var pm = new RegExp(pMonth);
 	var py = new RegExp(pYear);
 	
-	while ((pd.exec(value)) || (pm.exec(value)) || (py.exec(value)))
+	while ((md = pd.exec(value)) || (mm = pm.exec(value)) || (my = py.exec(value)))
 	{
 		d = getNewDate(getAddOn(pDay,value));
-		value = value.replace(pDay , (d.getDate()<10)?"0"+d.getDate():d.getDate());
-		month = d.getMonth()+1;
-		value = value.replace(pMonth , (month<10)?"0"+month: month);
-		value = value.replace(pYear , d.getFullYear());
+		if (md)	{
+			value = value.replace(pDay , (d.getDate()<10)?"0"+d.getDate():d.getDate());
+		}
+		if (mm) {
+			month = d.getMonth()+1;
+			value = value.replace(pMonth , (month<10)?"0"+month: month);
+		}
+		if (my) {
+			value = value.replace(pYear , d.getFullYear());
+		}
 	}
-	
 	return value;
 }
 
 function getNewDate(days){
 	var now = new Date();
-	var daylength= 1*24*60*60*1000;
-	var then = new Date(now*1+daylength*days);
-	return then;
+	var daylength= 86400000; //1*24*60*60*1000;
+	return new Date(now*1+daylength*days);
 }
 
 function getAddOn(pattern, value) {
 	pattern.exec(value);
 	match = RegExp.$1;
-	match = match.replace(/[()]/g,"");
+	match = match.replace(/\+/,"");
 	return match;
 }
